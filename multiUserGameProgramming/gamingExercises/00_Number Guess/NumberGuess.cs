ad94c9b13c286d2688f15fd5e281d8217fbe41b1
Kyomari B <90330125+Zonic21342@users.noreply.github.com>
2023-09-29 14:29:21 -0400

    V0.5
    Had to start over

--- multiUserGameProgramming/gamingExercises/00_Number Guess/NumberGuess.cs
+++ multiUserGameProgramming/gamingExercises/00_Number Guess/NumberGuess.cs
@@ -1,75 +1,43 @@
-// Kyomari Brunswick, NumberGuess, v0.04
+// Kyomari Brunswick, Number Guess, v0.5
 /*
-Generate secret number from a defined of numbers (ex:1-10)
-Print game instructions including range and number of guesses allowed.
-    MATCH == first player to score 3 points.
-    ROUND == guessing a specific number, until correct or no more attempts
+Generate secret number from a defined range of numbers (0-10, 0-50, 0-100)
+Print game instructions including range and num. of guesses allowed.
+    Match == first player to score 3 points.
+    Round == guessing a specific number, until correct or no more attempts
 Ask the player what difficulty they want to play on.
 Ask the player what their guess is.
 Determine if guess is correct or not.
-    If guess is correct
-    {
-        Tell them they have guesses correctly.
-        Give the player a point.
-    }else
-    {
+    if guess is correct {
+        Tell them they have guessed correctly.
+        Award the player a point.
+    } else {
         Tell them they are wrong.
         Tell player if guess is too high or too low.
-        Check to see if they have guesses remaining
-        {
-            if guess remain:
-            {
+        check to see if they have guesses remaining {
+            if guess remain: {
                 allow player to guess again.
-            }else
-            {
+            } else {
                 CPU wins the round.
             }
         }
     }
 
 */
-using System;
+using Sytem;
 
-namespace NumberGuess
+namespace numberGuess
 {
-    class NumberGuess
+    class numberGuess
     {
         static void Main(string[] args)
         {
             int secretNumber = -1;
-            int numGuesses = 0; // Number of guesses player is ALLOWED.
-            int  numAttemps = 0; // NUMBERS of guesses TAKEN.
-            int playerScore = 0;
-            int cpuScore = 0;
+            int numGuesses = 0; // NUMBER OF GUESSES PLAYER IS ALLOWED
+            int numAttempts =0; // NUMBER OF GUESSES TAKEN.
+            int playerGuess =0;
+            int playerScore =0;
+            int cpuScore =0;
             string difficulty = "";
-            int rangeMin = -1;
-            int rangeMax = -1;
-
-            Console.WriteLine("Number Guess Game\nSelect your difficulty next.\n");
-            Console.WriteLine("Easy mode: 0 - 10 with 4 guesses.\nNorml mode: Range is o - 25 with 4 guesses.")
-
-            // DIFFICULTY SLECTION
-            Console.WriteLine("enter Easy, Normal, or hard.")
-            difficulty = Console.ReadLine();
-            //Console
-            Console.WriteLine("Minimum: " + rangeMin);
-            Console.WriteLine("Maximum: " + rangeMax);
-            Console.WriteLine("Num. Guesses: " + numGuesses);
-
-            // START MATCH
-            while (playerScore != 3 && cpuScore != 3)
-            {
-                // Any code you want to run BEFORE each round goes here.
-
-                
-                // START EACH ROUND
-                for (int i - 0; i < numGuesses ; i++)
-                {
-                   // Code to guess number goes here.
-
-                }
-                
-            }
         }
     }
 }
