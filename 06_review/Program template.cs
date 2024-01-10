5fc8d88135c3d0179b34eea4c8acede007f24170
Kyomari B <90330125+Zonic21342@users.noreply.github.com>
2024-01-10 14:06:44 -0500

    v0.01
    first start

---
+++ 06_review/Program template.cs
@@ -0,0 +1,42 @@
+// Kyomari Brunswick, Program Template, v0.01
+using System;
+
+namespace ReviewProject
+{
+    class ReviewProject
+    {
+        static void Main(string[] args)
+        {
+
+            // Declaring and Assigning Variables
+            // Integer -- -2,147,483,648 to 2,147,483,647
+            // Integer = 4 Bytes
+            Int myInt = 5;
+
+            // Long -- -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+            // Long = 8 Bytes
+            Long myLong = 1928742747242904;
+
+            // Float -- Stores up to 6-7 decimal digits.
+            // Float = 4 Bytes
+            float myFloat = 3.1432145F;
+
+            // Double -- Stores up to 15 decimal digits.
+            // Double = 8 Bytes
+
+            // Boolean -- true / false
+            // Boolean = 1 bit
+            bool myBool = true;
+
+            // String
+            // 2-bytes PER CHARACTER
+            // Must be in DOUBLE QUOTES " "
+            string myString = "Peanut Butter Jelly Time";
+
+            // Character (char)
+            // Single character, in SINGLE QUOTES ' '
+            // 2-bytes
+            char myChar = 'C';
+        }
+    }
+}
